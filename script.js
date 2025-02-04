a3123497e381dccd05d8e3f2394d3b1912aa6292
Amaan1911 <163126376+Amaan1911@users.noreply.github.com>
2025-02-04 22:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function convertToRoman(num) {
-  	const romanNumerals = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const romanNumerals = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
     };
 
-  //your code here
-	    let result = "";
-	  const values = Object.values(romanNumerals).sort((a, b) => b[1] - a[1]);
+    let result = "";
+
+    // Convert object values into an array and sort them in descending order
+    const values = Object.values(romanNumerals).sort((a, b) => b[1] - a[1]);
 
     for (let i = 0; i < values.length; i++) {
         while (num >= values[i][1]) {
@@ -21,15 +22,4 @@ function convertToRoman(num) {
     }
 
     return result;
-
-
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
